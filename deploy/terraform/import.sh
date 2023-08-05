df02df710c7aaaf334c65232d6f0af85a26ca27c
Craig Miller <craigmiller160@gmail.com>
2023-08-05 11:32:02 -0400

    Working on import script

--- deploy/terraform/import.sh
+++ deploy/terraform/import.sh
@@ -14,3 +14,18 @@ function plan {
     -var="env=prod"
 }
 
+import "nexus_blobstore_file.npm_private" "npm-private"
+import "nexus_blobstore_file.npm_proxy" "npm-proxy"
+import "nexus_blobstore_file.npm_group" "npm-group"
+import "nexus_blobstore_file.docker_private" "docker-private"
+import "nexus_blobstore_file.docker_proxy" "docker-proxy"
+import "nexus_blobstore_file.docker_group" "docker-group"
+import "nexus_blobstore_file.helm_private" "helm-private"
+
+import "nexus_repository_npm_hosted.npm_private" "npm-private"
+import "nexus_repository_npm_proxy.npm_proxy" "npm-proxy"
+import "nexus_repository_npm_group.npm_group" "npm-group"
+import "nexus_repository_docker_hosted.docker_private" "docker-private"
+import "nexus_repository_docker_proxy.docker_proxy" "docker-proxy"
+import "nexus_repository_docker_group.docker_group" "docker-group"
+import "nexus_repository_helm_hosted.helm_private" "helm-private"
